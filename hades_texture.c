ae2151fce1bbdd7acd34b51cafb709e4b104b540
Josie Thompson <josiahst@gmail.com>
2018-11-10 22:57:06 -0800

    forgot to add texture files

---
+++ hades_texture.c
@@ -0,0 +1,52 @@
+/** hades_texture.c
+ */
+
+/* Table of Contents
+ *
+ *  Hades_GetTextureCount
+ *  Hades_LoadTexture
+ */
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "hades_game.h"
+#include "hades_texture.h"
+#include "hades_bool.h"
+#include "hades_error.h"
+
+size_t Hades_GetTextureCount(Hades_Game* game)
+{
+    if (!game) {
+        return 0;
+    }
+    return game->texture_count;
+}
+
+Hades_bool Hades_LoadTexture(Hades_Game* game, const char* path)
+{
+    if (!game) {
+        return Hades_false;
+    }
+    if (game->texture_count == Hades_MaxTextureCount) {
+        Hades_SetGameError("Reached max texture count. Cannot load more");
+        return Hades_false;
+    }
+
+    SDL_Surface* loaded_surface = IMG_Load(path);
+    if (loaded_surface == NULL) {
+        Hades_SetErrorIMG("Could not load image");
+        return Hades_false;
+    }
+
+    SDL_Texture*
+    texture = SDL_CreateTextureFromSurface(game->renderer, loaded_surface);
+    SDL_FreeSurface(loaded_surface);
+    if (!texture) {
+        Hades_SetErrorSDL("Unable to create texture from loaded surface");
+        return Hades_false;
+    }
+
+    game->textures[game->texture_count] = texture;
+    game->texture_count += 1;
+    return Hades_true;
+}
---
+++ hades_texture.h
@@ -0,0 +1,56 @@
+/** hades_texture.h
+ * Hades interface for loading textures
+ */
+
+/* Table of Contents
+ *
+ *  Hades_GetTextureCount    18
+ *  Hades_LoadTexture        33
+ */
+
+#ifndef HADES_TEXTURE_
+#define HADES_TEXTURE_
+
+#include <stddef.h>
+#include "hades_game.h"
+#include "hades_bool.h"
+
+/** Defined in "hades_texture.h"
+ * Hades_GetTextureCount
+ *  Get the amount of texture currently loaded
+ *
+ * Parameters
+ *  game - game to query
+ *
+ * Preconditions
+ *  all textures have been added via Hades_LoadTexture
+ *
+ * Postconditions
+ *  if game is null, returns 0
+ */
+size_t Hades_GetTextureCount(Hades_Game*);
+
+/** Defined in "hades_texture.h"
+ * Hades_LoadTexture
+ *  Load a texture into a game
+ *
+ * Parameters
+ *  game - the game to load the texture into
+ *  path - the path of the image to load
+ *
+ * Preconditions
+ *  game has been initialized*
+ *  image in path is png
+ *
+ * Postconditions
+ *  If successful, GetTextureCount updates texture count and returns true.
+ *  The index used for the texture is the same as texture count before
+ *  updating. The user is responsible for keeping track of what indeces
+ *  correspond to what images.
+ *
+ *  If GetTextureCount fails, it will return false and set the corresponding
+ *  error message.
+ */
+Hades_bool Hades_LoadTexture(Hades_Game*, const char*);
+
+#endif
