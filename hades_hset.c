6d53e3ef4f6f7a54500b18a7b41166debc31a9da
Josie Thompson <josiahst@gmail.com>
2019-01-23 10:02:04 -0800

    implemented hset and integrated it into object collisions

---
+++ hades_hset.c
@@ -0,0 +1,151 @@
+#include "hades_hset.h"
+#include "hades_stdhash.h"
+#include "hades_iter.h"
+#include "hades_error.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+Hades_HSet* Hades_NewHSet(Hades_SizeFunc HashItem, Hades_BiPredicate ItemEq,
+                          Hades_Consumer FreeItem)
+{
+    
+    Hades_HSet* set = (Hades_HSet*) malloc(sizeof(Hades_HSet));
+    if (NULL == set) {
+        Hades_SetGameError("Ran out of memory when creating a set");
+        return NULL;
+    }
+    const Hades_HSet setcpy = {
+        0, {NULL}, HashItem? HashItem : Hades_HashID,
+        ItemEq? ItemEq : Hades_IDEq, FreeItem? FreeItem : free
+    };
+    memcpy(set, &setcpy, sizeof(Hades_HSet));
+    return set;
+}
+
+void Hades_FreeHSet(Hades_HSet* set)
+{
+    if (NULL == set) {
+        Hades_SetGameError("FreeHSet was called with a null object");
+    }
+    for (int i = 0; i < Hades_MaxBuckets; i++) {
+        Hades_HSetNode* top = set->buckets[i];
+        while (top) {
+            set->FreeItem(top->item);
+            Hades_HSetNode* tmp = top;
+            top = top->next;
+            free(tmp);
+        }
+    }
+    free(set);
+}
+
+Hades_HSetNode* Hades_GetNodeFromHSet(const Hades_HSet* set, const void* item)
+{
+    size_t i = set->HashItem(item) % Hades_MaxBuckets;
+    Hades_HSetNode* node = set->buckets[i];
+    while (node && !set->ItemEq(item, node->item)) {
+        node = node->next;
+    }
+    return node;
+}
+
+bool Hades_AddToHSet(Hades_HSet* set, const void* item, size_t size)
+{
+    if (NULL == set) {
+        Hades_SetGameError("AddToHSet called with a null set");
+        return false;
+    }
+    Hades_HSetNode* node = Hades_GetNodeFromHSet(set, item);
+    if (node) {
+        return false;
+    }
+    size_t i = set->HashItem(item) % Hades_MaxBuckets;
+    Hades_HSetNode nodecpy = {malloc(size), NULL, set->buckets[i]};
+    if (nodecpy.item != NULL) {
+        memcpy(&nodecpy.item, item, size);
+    } else {
+        Hades_SetGameError("Ran out of memory when adding to a set");
+        return false;
+    }
+    set->buckets[i] = (Hades_HSetNode*) malloc(sizeof(Hades_HSetNode));
+    if (set->buckets[i] != NULL) {
+        memcpy(set->buckets[i], &nodecpy, sizeof(Hades_HSetNode));
+        set->size += 1;
+    } else {
+        Hades_SetGameError("Ran out of memory when creating a set");
+        set->buckets[i] = nodecpy.next;
+        return false;
+    }
+    return true;
+}
+
+bool Hades_HSetHasItem(const Hades_HSet* set, const void* item)
+{
+    if (NULL == set) {
+        Hades_SetGameError("HSetHasItem called with null set");
+        return false;
+    }
+    Hades_HSetNode* node = Hades_GetNodeFromHSet(set, item);
+    return node != NULL;
+}
+
+void* Hades_RmFromHSet(Hades_HSet* set, const void* key)
+{
+    if (NULL == set) {
+        Hades_SetGameError("RmFromHSet called with null set");
+        return NULL;
+    }
+    Hades_HSetNode* node = Hades_GetNodeFromHSet(set, key);
+    if (!node) {
+        return NULL;
+    }
+    if (node->prev) {
+        node->prev->next = node->next;
+    } else {
+        set->buckets[set->HashItem(key) % Hades_MaxBuckets] = node->next;
+    }
+    if (node->next) {
+        node->next->prev = node->prev;
+    }
+    void* item = node->item;
+    free(node);
+    set->size -= 1;
+    return item;
+}
+
+Hades_Iter* Hades_IterHSet(const Hades_HSet* set)
+{
+    if (NULL == set) {
+        Hades_SetGameError("IterHSet called with null set");
+        return NULL;
+    }
+    Hades_Iter* iter = (Hades_Iter*) malloc(sizeof(Hades_Iter));
+    if (iter != NULL) {
+        Hades_Iter itercpy = {NULL, NULL};
+        memcpy(iter, &itercpy, sizeof(Hades_Iter));
+    } else {
+        Hades_SetGameError("Ran out of memory when creating a set iterator");
+        return NULL;
+    }
+
+    for (int i = 0; i < Hades_MaxBuckets; i++) {
+        Hades_HSetNode* curr = set->buckets[i];
+        while (curr != NULL) {
+            const Hades_IterNode nodecpy = {curr->item, iter->top};
+            iter->top = (Hades_IterNode*) malloc(sizeof(Hades_IterNode));
+            if (iter->top != NULL) {
+                memcpy(iter->top, &nodecpy, sizeof(Hades_IterNode));
+                curr = curr->next;
+            } else {
+                Hades_SetGameError("Ran out of memory when creating a set "
+                                   "iterator");
+                iter->top = nodecpy.next;
+                Hades_CloseIter(iter);
+                return NULL;
+            }
+        }
+    }
+    iter->curr = iter->top;
+    return iter;
+}
---
+++ hades_hset.h
@@ -0,0 +1,143 @@
+#ifndef HADES_HSET_H
+#define HADES_HSET_H
+
+#include "hades_stdhash.h"
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct Hades_HSet Hades_HSet;
+typedef struct Hades_HSetNode Hades_HSetNode;
+typedef struct Hades_Iter Hades_Iter;
+
+typedef void (*Hades_Consumer)(void*);
+typedef size_t (*Hades_SizeFunc)(const void*);
+typedef bool (*Hades_BiPredicate)(const void*, const void*);
+
+/** defined in "hades_hset.h"
+ * struct Hades_HSet;
+ *  A quasi-generic hash set.
+ *
+ * Fields
+ *                size_t size - of the set
+ *  Hades_HSetNode* buckets[] - stores the items
+ *    Hades_SizeFunc HashItem - hash an item
+ *   Hades_BiPredicate ItemEq - compare two items for equality
+ *    Hades_Consumer FreeItem - deallocate item
+ */
+struct Hades_HSet {
+    size_t size;
+    Hades_HSetNode* buckets[Hades_MaxBuckets];
+    Hades_SizeFunc HashItem;
+    Hades_BiPredicate ItemEq;
+    Hades_Consumer FreeItem;
+};
+
+/** defined in "hades_hset.h"
+ * struct Hades_HSetNode;
+ *  stores an item for a set
+ *
+ * Fields
+ *      void* item - being stored
+ *  Hades_HSetNode* prev - prev bucket
+ *  Hades_HSetNode* next - next bucket
+ */
+struct Hades_HSetNode {
+    void* item;
+    Hades_HSetNode* prev;
+    Hades_HSetNode* next;
+};
+
+/** defined in "hades_hset.h"
+ * Hades_HSet* Hades_NewHSet(Hades_SizeFunc HashItem,
+ *                           Hades_BiPredicate ItemEq,
+ *                           Hades_Consumer FreeItem);
+ *  Create a new hash set.
+ *
+ * Parameters:
+ *  HashItem - hash an item
+ *    ItemEq - compare two items for equality
+ *  FreeItem - deallocate an item
+ *
+ * Postconditions:
+ *  If HashItem is null, HashID will be used. If ItemEq is Null, IDEq will
+ *  be used. If FreeItem is null, free will be used.
+ */
+Hades_HSet* Hades_NewHSet(Hades_SizeFunc, Hades_BiPredicate, Hades_Consumer);
+
+/** defined in "hades_hset.h"
+ * void Hades_FreeHSet(Hades_HSet* set);
+ *  Deallocate set.
+ *
+ * Parameters:
+ *  set - to deallocate
+ *
+ * Preconditions:
+ *  set isn't null
+ */
+void Hades_FreeHSet(Hades_HSet*);
+
+/** defined in "hades_hset.h"
+ * Hades_HSetNode* Hades_GetNodeFromHSet(const Hades_HSet* set,
+ *                                       const void* item);
+ *  Get a node from a set.
+ *
+ * Parameters:
+ *   set - contains item
+ *  item - to get the node of
+ *
+ * Postconditions:
+ *  Returns null if item doesn't exist in set.
+ */
+Hades_HSetNode* Hades_GetNodeFromHSet(const Hades_HSet*, const void*);
+
+/** defined in "hades_hset.h"
+ * bool Hades_AddToHSet(Hades_HSet* set, const void* item, size_t size);
+ *  Add item to set.
+ *
+ * Parameters:
+ *   set - to add to
+ *  item - to add
+ *  size - of item to add
+ *
+ * Postconditions:
+ *  Returns true if item didn't previously exist in set and shallow copies
+ *  item.
+ */
+bool Hades_AddToHSet(Hades_HSet* set, const void* item, size_t size);
+
+/** defined in "hades_hset.h"
+ * bool Hades_HSetHasItem(const Hades_HSet* set, const void* item);
+ *  Determine if item exists in set.
+ *
+ * Parameters:
+ *   set - to check in
+ *  item - to look for
+ */
+bool Hades_HSetHasItem(const Hades_HSet*, const void*);
+
+/** defined in "hades_hset.h"
+ * void* Hades_RmItemFromHSet(Hades_HSet* set, const void* item);
+ *  Remove item from set.
+ *
+ * Parameters:
+ *   set - to remove from
+ *  item - to remove
+ *
+ * Postconditions:
+ *  Returns null if item doesn't exist in set.
+ */
+void* Hades_RmFromHSet(Hades_HSet*, const void*);
+
+/** defined in "hades_hset.h"
+ * Hades_Iter* Hades_IterHSet(const Hades_HSet* set);
+ *  Iterate set.
+ *
+ * Parameters:
+ *  set - to iterate
+ *
+ * Postconditions:
+ *  Iterates over pointers to items.
+ */
+Hades_Iter* Hades_IterHSet(const Hades_HSet*);
+
+#endif
